cca6111f238725e0d31e3318f7c2b81e2e252b36
Don Coleman <dcoleman@chariotsolutions.com>
2015-06-26 17:12:35 -0700

    initial import

---
+++ arduino/Weather/Weather.ino
@@ -0,0 +1,87 @@
+// Weather is a bad name unless we add Barometric pressure
+
+#include <SPI.h>
+#include <BLEPeripheral.h>
+
+// define pins for Adafruit Bluefruit LE 
+// https://github.com/sandeepmistry/arduino-BLEPeripheral#pinouts
+#define BLE_REQ 10
+#define BLE_RDY 2
+#define BLE_RST 9
+
+BLEPeripheral blePeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
+BLEService thermometerService = BLEService("BBB0");
+// TODO normalize descriptor text Temperature, Humidity, Pressure
+// TODO are there standard Bluetooth descriptors for units?
+BLEFloatCharacteristic temperatureCharacteristic = BLEFloatCharacteristic("BBB1", BLERead | BLENotify);
+BLEDescriptor temperatureDescriptor = BLEDescriptor("2901", "Temperature");
+BLEFloatCharacteristic humidityCharacteristic = BLEFloatCharacteristic("BBB2", BLERead | BLENotify);
+BLEDescriptor humidityDescriptor = BLEDescriptor("2901", "Humidity");
+BLEFloatCharacteristic pressureCharacteristic = BLEFloatCharacteristic("BBB3", BLERead | BLENotify);
+BLEDescriptor pressureDescriptor = BLEDescriptor("2901", "Pressure");
+
+#include "DHT.h"
+#define DHTPIN 7        // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+// TODO add pressure sensor
+
+long previousMillis = 0;  // will store last time sensor was read
+long interval = 2000;     // interval at which to read sensor (milliseconds)
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("Bluetooth Low Energy Weather Station"));
+  
+  // set advertised name and service
+  blePeripheral.setLocalName("Weather");
+  blePeripheral.setDeviceName("Weather");
+  blePeripheral.setAdvertisedServiceUuid(thermometerService.uuid());
+
+  // add service and characteristic
+  blePeripheral.addAttribute(thermometerService);
+  blePeripheral.addAttribute(temperatureCharacteristic);
+  blePeripheral.addAttribute(temperatureDescriptor);
+  blePeripheral.addAttribute(humidityCharacteristic);
+  blePeripheral.addAttribute(humidityDescriptor);
+  blePeripheral.addAttribute(pressureCharacteristic);
+  blePeripheral.addAttribute(pressureDescriptor);
+  
+  blePeripheral.begin();
+}
+
+void loop()
+{
+  // Tell the bluetooth radio to do whatever it should be working on
+  blePeripheral.poll();
+  
+  // limit how often we read the sensor
+  if(millis() - previousMillis > interval) {
+    pollSensor();
+    previousMillis = millis();
+  }
+}
+
+void pollSensor()
+{
+  
+  float temperature = dht.readTemperature(true);
+  float humidity = dht.readHumidity();
+
+  // only set the characteristic value if the temperature has changed
+  if (temperatureCharacteristic.value() != temperature) {
+    temperatureCharacteristic.setValue(temperature);
+    Serial.print("Temperature ");
+    Serial.println(temperature);
+  }
+
+  if (humidityCharacteristic.value() != humidity) {
+    humidityCharacteristic.setValue(humidity);
+    Serial.print("Humidity ");
+    Serial.println(humidity);
+  }
+
+}
+
---
+++ phonegap/www/js/index.js
@@ -0,0 +1,129 @@
+var THERMOMETER_SERVICE = 'BBB0';
+var TEMPERATURE_CHARACTERISTIC = 'BBB1';
+var HUMIDITY_CHARACTERISTIC = 'BBB2';
+var PRESSURE_CHARACTERISTIC = 'BBB3';
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+        this.showMainPage();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        deviceList.addEventListener('click', this.connect, false);
+        refreshButton.addEventListener('click', this.refreshDeviceList, false);
+        disconnectButton.addEventListener('click', this.disconnect, false);
+    },
+    onDeviceReady: function() {
+        app.refreshDeviceList();
+    },
+    refreshDeviceList: function() {
+        deviceList.innerHTML = ''; // empty the list
+        ble.scan([THERMOMETER_SERVICE], 5, app.onDiscoverDevice, app.onError);
+    },
+    onDiscoverDevice: function(device) {
+       var listItem = document.createElement('li');
+       listItem.innerHTML = device.name + ' ' + device.id;
+       listItem.dataset.deviceId = device.id;
+       deviceList.appendChild(listItem);
+    },
+    connect: function(e) {
+        var deviceId = e.target.dataset.deviceId;
+        ble.connect(deviceId, app.onConnect, app.onError);
+    },
+    onConnect: function(peripheral) {
+        app.peripheral = peripheral;
+        app.showDetailPage();
+
+        var failure = function(reason) {
+           navigator.notification.alert(reason, null, "Temperature Error");
+        };
+
+        // subscribe to be notified when the button state changes
+        ble.startNotification(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            TEMPERATURE_CHARACTERISTIC,
+            app.onTemperatureChange,
+            failure
+        );
+
+        // subscribe to be notified when the button state changes
+        ble.startNotification(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            HUMIDITY_CHARACTERISTIC,
+            app.onHumidityChange,
+            failure
+        );
+
+        // subscribe to be notified when the button state changes
+        ble.startNotification(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            PRESSURE_CHARACTERISTIC,
+            app.onPressureChange,
+            failure
+        );
+
+        // read the initial value
+        ble.read(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            TEMPERATURE_CHARACTERISTIC,
+            app.onTemperatureChange,
+            failure
+        );
+
+        ble.read(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            HUMIDITY_CHARACTERISTIC,
+            app.onHumidityChange,
+            failure
+        );
+
+        ble.read(
+            peripheral.id,
+            THERMOMETER_SERVICE,
+            PRESSURE_CHARACTERISTIC,
+            app.onPressureChange,
+            failure
+        );
+
+    },
+    onTemperatureChange: function(buffer) {
+        var data = new Float32Array(buffer);
+        var temperature = data[0];
+        var message = "Temperature is " + temperature.toFixed(1) + "&deg;F";
+        temperatureDiv.innerHTML = message;
+    },
+    onHumidityChange: function(buffer) {
+        var data = new Float32Array(buffer);
+        var humidity = data[0];
+        var message = "Humidity is " + humidity.toFixed(1) + "%";
+        humidityDiv.innerHTML = message;
+    },
+    onPressure: function(buffer) {
+        var data = new Float32Array(buffer);
+        var pressure = data[0];
+        var message = "Pressure is " + pressure.toFixed(1) + " in";
+        pressureDiv.innerHTML = message;
+    },
+    disconnect: function(e) {
+        if (app.peripheral && app.peripheral.id) {
+            ble.disconnect(app.peripheral.id, app.showMainPage, app.onError);
+        }
+    },
+    showMainPage: function() {
+        mainPage.hidden = false;
+        detailPage.hidden = true;
+    },
+    showDetailPage: function() {
+        mainPage.hidden = true;
+        detailPage.hidden = false;
+    },
+    onError: function(reason) {
+       navigator.notification.alert(reason, app.showMainPage, "Error");
+    }
+};
