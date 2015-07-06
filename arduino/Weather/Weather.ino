751d4c516be3ef74c9bf47f3c4a459f65cf56cbe
Don Coleman <dcoleman@chariotsolutions.com>
2015-07-05 20:56:20 -0400

    remove display descriptor (not enough memory)

--- arduino/Weather/Weather.ino
+++ arduino/Weather/Weather.ino
@@ -10,20 +10,11 @@
 BLEPeripheral blePeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
 BLEService weatherService = BLEService("BBB0");
 BLEFloatCharacteristic temperatureCharacteristic = BLEFloatCharacteristic("BBB1", BLERead | BLENotify);
-//BLEDescriptor temperatureDescriptor = BLEDescriptor("2901", "Temperature (deg F)");
+BLEDescriptor temperatureDescriptor = BLEDescriptor("2901", "Temperature (deg F)");
 BLEFloatCharacteristic humidityCharacteristic = BLEFloatCharacteristic("BBB2", BLERead | BLENotify);
-//BLEDescriptor humidityDescriptor = BLEDescriptor("2901", "Humidity (%)");
+BLEDescriptor humidityDescriptor = BLEDescriptor("2901", "Humidity (%)");
 BLEUnsignedLongCharacteristic pressureCharacteristic = BLEUnsignedLongCharacteristic("BBB3", BLERead | BLENotify);
-BLEDescriptor pressureDescriptor = BLEDescriptor("2901", "Pressure"); // (pascal)
-
-// Display Format https://developer.bluetooth.org/gatt/descriptors/Pages/DescriptorViewer.aspx?u=org.bluetooth.descriptor.gatt.characteristic_presentation_format.xml
-// format - 8 (32-bit unsigned integer)
-// exponent - 0
-// unit - 0x2724 pascal
-// namespace - 1 Bluetooth SIG
-// namespace description - 0000
-const uint8_t displayFormat[] = { 8, 0, 0x24, 0x27, 1, 0, 0 };
-BLEDescriptor pressureDisplayDescriptor = BLEDescriptor("2904", displayFormat, sizeof(displayFormat));
+BLEDescriptor pressureDescriptor = BLEDescriptor("2901", "Pressure (pascal)");
 
 #include "DHT.h"
 #define DHTPIN 7        // what pin we're connected to
@@ -34,7 +25,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 
-long previousMillis = 0;  // will store last time sensor was read
+long previousMillis = 0;  // stores the last time sensor was read
 long interval = 2000;     // interval at which to read sensor (milliseconds)
 
 void setup()
@@ -55,12 +46,11 @@ void setup()
   // add service and characteristic
   blePeripheral.addAttribute(weatherService);
   blePeripheral.addAttribute(temperatureCharacteristic);
-//  blePeripheral.addAttribute(temperatureDescriptor);
+  blePeripheral.addAttribute(temperatureDescriptor);
   blePeripheral.addAttribute(humidityCharacteristic);
-//  blePeripheral.addAttribute(humidityDescriptor);
+  blePeripheral.addAttribute(humidityDescriptor);
   blePeripheral.addAttribute(pressureCharacteristic);
   blePeripheral.addAttribute(pressureDescriptor);
-  blePeripheral.addAttribute(pressureDisplayDescriptor);
 
   blePeripheral.begin();
 }
@@ -80,23 +70,9 @@ void loop()
 void pollSensors()
 {
 
-  Serial.println(F("Polling"));
   float temperature = dht.readTemperature(true); // fahrenheight
   float humidity = dht.readHumidity();
-
-  // http://www.srh.noaa.gov/images/epz/wxcalc/pressureConversion.pdf
-  // To convert between inches of mercury (inHg) and millibars (mb) or hectopascals (hPa)
-  // P inHg = 0.0295300 x P mb
-  // Dividing by 100 to convert pascals to hectopascals
-  //float pressure = bmp.readPressure() * 0.0295300 / 100.0;
   int32_t pressure = bmp.readPressure();
-
-  if (isnan(temperature)) {
-    Serial.println(F("Temp NaN"));
-  }
-  if (isnan(humidity)) {
-    Serial.println(F("Humidity NaN"));
-  }
 
   // only set the characteristic value if the temperature has changed
   if (!isnan(temperature) && temperatureCharacteristic.value() != temperature) {
